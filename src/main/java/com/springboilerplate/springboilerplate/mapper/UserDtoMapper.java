f3525aacc535ba8877ae63b38ea0af4f47a8b34b
Patrick <inem.patrick@gmail.com>
2017-10-23 13:28:49 +0100

    resolve encoding issue

--- src/main/java/com/springboilerplate/springboilerplate/mapper/UserDtoMapper.java
+++ src/main/java/com/springboilerplate/springboilerplate/mapper/UserDtoMapper.java
@@ -1,3 +1,4 @@
+
 package com.springboilerplate.springboilerplate.mapper;
 
 import com.springboilerplate.springboilerplate.dto.UserDto;
--- src/main/java/com/springboilerplate/springboilerplate/model/Role.java
+++ src/main/java/com/springboilerplate/springboilerplate/model/Role.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 @Entity
 @Table(name="roles")
