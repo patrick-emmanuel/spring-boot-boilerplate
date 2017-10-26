3faef07432455aa5cc64261da0356b15004e41a2
Patrick <inem.patrick@gmail.com>
2017-10-26 17:22:41 +0100

    remove elastic search

--- src/main/java/com/springboilerplate/springboilerplate/elasticSearch/elasticQueryBuilder/EsUserQueryBuilder.java
+++
@@ -1,9 +0,0 @@
-package com.springboilerplate.springboilerplate.elasticSearch.elasticQueryBuilder;
-
-import com.springboilerplate.springboilerplate.elasticSearch.elasticModel.EsUser;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface EsUserQueryBuilder {
-    Page<EsUser> findAllUsers(String text, Pageable pageable);
-}
